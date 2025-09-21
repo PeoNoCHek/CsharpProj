e24a4d5b9d67e1c745ddf838a6cf19a63217e0cb
Penb <PeoplesKnowes@gmail.com>
2025-09-21 18:22:12 +0300

    Работает загрузка из файла. Пока что всё сырое.

--- AlgorithmPR1/Program.cs
+++ AlgorithmPR1/Program.cs
@@ -2,30 +2,50 @@
 // See https://aka.ms/new-console-template for more information
 
 
-// String[] lines = File.ReadAllLines(@"D:\Алгоритмы\pr1\names.txt");
-// List<(int number, String name, int money)> list = new List<(int number, string name, int money)>();
+String[] lines = File.ReadAllLines(@"/home/penb/CsharpProj/AlgorithmPR1/names_old.txt");
+List<(int number, String name, int money)> list = new List<(int number, string name, int money)>();
 
-// for (int i = 0; i < lines.Length; i++) {
-//     var temp = lines[i].Split(' ');
-//     list.Add((i, temp[0], Int32.Parse(temp[1])));
-// }
+for (int i = 0; i < lines.Length; i++) {
+    var temp = lines[i].Split(' ');
+    list.Add((i, temp[0], Int32.Parse(temp[1])));
+}
 
-// foreach (var n in list)
-// {
-//     System.Console.WriteLine(n);
-// }
+//test
+Dictionary<int, Tree> tree_dictionary = new Dictionary<int, Tree>();
+foreach (var (number, name, money) in list)
+{
+    tree_dictionary[number] = new Tree(name, money, number);
+}
 
-Tree example = new Tree("Михаил",100,0);
-example.AddVertex("Лев", 20, 1);
-example.vertexes[0].AddVertex("Злата", 12, 4);
-example.vertexes[0].vertexes[0].AddVertex("Дарина",12,6);
-example.vertexes[0].vertexes[0].AddVertex("Сергей",42,7);
-example.vertexes[0].vertexes[0].vertexes[0].AddVertex("Анна",122,9);
-example.AddVertex("Надежда", 40, 2);
-example.vertexes[1].AddVertex("Мария", 23, 5);
-example.vertexes[1].vertexes[0].AddVertex("Валерия", 12, 8);
-example.vertexes[1].vertexes[0].vertexes[0].AddVertex("Ярослава", 2, 10);
-example.AddVertex("Илья", 45, 3);
-example.PrintTree();
+//test
 
+//test0
+foreach (var line in File.ReadAllLines(@"/home/penb/CsharpProj/AlgorithmPR1/position.txt"))
+{
+    var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToList();
+    int boss = parts[0];
 
+    for (int i = 1; i < parts.Count; i++)
+    {
+        int sub = parts[i];
+        tree_dictionary[boss].vertexes.Add(tree_dictionary[sub]);
+        tree_dictionary[sub].parent = tree_dictionary[boss];
+    }
+}
+//test0
+
+Tree root = tree_dictionary.Values.First(n=>n.parent == null);
+root.PrintTree();
+
+// Tree0 example = new Tree0("Михаил",100,0);
+// example.AddVertex(list);
+// example.AddVertex("Лев", 20, 1);
+// example.vertexes[0].AddVertex("Злата", 12, 4);
+// example.vertexes[0].vertexes[0].AddVertex("Дарина",12,6);
+// example.vertexes[0].vertexes[0].AddVertex("Сергей",42,7);
+// example.vertexes[0].vertexes[0].vertexes[0].AddVertex("Анна",122,9);
+// example.AddVertex("Надежда", 40, 2);
+// example.vertexes[1].AddVertex("Мария", 23, 5);
+// example.vertexes[1].vertexes[0].AddVertex("Валерия", 12, 8);
+// example.vertexes[1].vertexes[0].vertexes[0].AddVertex("Ярослава", 2, 10);
+// example.AddVertex("Илья", 45, 3);
--- AlgorithmPR1/TreeOld.cs
+++ AlgorithmPR1/TreeOld.cs
@@ -1,16 +1,15 @@
-public class Tree
+public class TreeOld
 {
-    public Tree parent;
-    public List<Tree> vertexes = new List<Tree>();
+    public TreeOld parent;
+    public List<TreeOld> vertexes = new List<TreeOld>();
     //эта хрень чисто для моей задачки
     public string name { get; private set; }
     public int money { get; private set; }
-    public int number { get; private set; }
+    public int number{ get; private set; }
 
     private bool _locked = false;
     //
-
-    public Tree(string name, int money, int number, Tree? parent = null)
+    public TreeOld(string name, int money, int number, TreeOld? parent = null)
     {
         this.name = name;
         this.money = money;
@@ -18,32 +17,18 @@ public class Tree
         this.parent = parent;
     }
 
-    public Tree AddVertex(String name, int money, int number)
+    public TreeOld AddVertex(String name, int money, int number)
     {
         if (_locked)
         {
             System.Console.WriteLine("Не удалось добавить: добавление закрыто");
             return null;
         }
-        var temp = new Tree(name, money, number, this);
+        var temp = new TreeOld(name, money, number, this);
         this.vertexes.Add(temp);
         return temp;
     }
 
-    public List<(int number, string name, int money)> AddVertex(List<(int number, string name, int money)> someaddition)
-    {
-        if (_locked)
-        {
-            System.Console.WriteLine("Не удалось добавить: добавление закрыто");
-            return null;
-        }
-        foreach ((int number, string name, int money) in someaddition)
-        {
-            this.vertexes.Add(new Tree(name,money,number,this));
-        }
-
-        return someaddition;
-    }
     public void Lock() => _locked = true;
 
     public void PrintTree(string indent = "", bool last = true)
@@ -55,6 +40,8 @@ public class Tree
     }
 
     public override string ToString() => $"{name} {money} {number}";
+
+
 }
 
 
